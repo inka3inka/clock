fd22b16829b2506168b32ce871247c19664328e0
inka3inka <inka3inka@gmail.com>
2020-04-03 17:47:35 +0200

    added change hour,s mode and readme

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ import {monthAsString, dayOfWeekAsString, changeCheckbox, showHide} from "./util
   const date = new Data(localDate);
 
   /*Find elements */
-  const showTimeHour = document.querySelector(".hour"); /*clock elements */
+  const showTimeHour24 = document.querySelector(".hour-24"); /*clock elements */
+  const showTimeHour12 = document.querySelector(".hour-12");
   const showTimeMinutes = document.querySelector(".minutes");
   const showTimeSeconds = document.querySelector(".seconds");
   const showTimeMilliseconds = document.querySelector(".milliseconds");
@@ -25,26 +26,38 @@ import {monthAsString, dayOfWeekAsString, changeCheckbox, showHide} from "./util
 setInterval(function(){
   clock = new Clock(localDate);
   console.log(clock);
-  showTimeHour.innerText = `${clock.hour}`;
+  showTimeHour24.innerText = `${clock.hour}`;
+  showTimeHour12.innerText = `${clock.hour - 12}`;
   showTimeMinutes.innerText = clock.minutes < 10 ? `:0${clock.minutes}` : `:${clock.minutes}`;
   showTimeSeconds.innerText = clock.seconds < 10 ? `:0${clock.seconds}` : `:${clock.seconds}`;
   showTimeMilliseconds.innerText = `:${clock.milliseconds}`;
 }, 10);
 
-  showDay.innerText = dayOfWeekAsString(consts.days, date.day);
-  showDate.innerText = `${date.date} ${monthAsString(consts.months, date.month)} ${date.year}`;
+  // showDay.innerText = dayOfWeekAsString(consts.days, date.day);
+  // showDate.innerText = `${date.date} ${monthAsString(consts.months, date.month)} ${date.year}`;
 
 /*Add event to menu elements */
 [...checkboxes].map(elm => elm.addEventListener("click", changeCheckbox));
 
   /*Hours*/
-  checkboxesArray[0].addEventListener("change", () => {
-    checkboxesArray[0].classList.contains("checked") && clock.hour > 12
-      ? showTimeHour.innerText = `${clock.hour-12}`
-      : showTimeHour.innerText = `${clock.hour}`
-  });
+  // checkboxesArray[0].addEventListener("change", () => {
+  //   checkboxesArray[0].classList.contains("checked")
+  //     ? showTimeHour.innerText = `${clock.hour-12}`
+  //     : showTimeHour.innerText = `${clock.hour}`
+  // });
 
 /*Show / hide element */
+
+  /*Hours' mode */
+
+  checkboxesArray[0].addEventListener("change", () => {
+    checkboxesArray[0].classList.contains("checked")
+        ? showTimeHour24.style.display = "none"
+        : showTimeHour24.style.display = "flex"
+    });
+
+  showHide(checkboxesArray[0], showTimeHour12);
+
   /*Minutes*/
   showHide(checkboxesArray[1], showTimeMinutes);
 
